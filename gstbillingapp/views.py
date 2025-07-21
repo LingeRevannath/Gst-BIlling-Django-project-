cde58f495a5f01af409386484550c1108461426b
Linge Revannath Hanumant <147597111+LingeRevannath@users.noreply.github.com>
2025-07-21 11:01:15 +0530

    Pushing project again to new repo

--- gstbillingapp/views.py
+++ gstbillingapp/views.py
@@ -44,16 +44,22 @@ from .forms import BookLogForm
 # User Management =====================================
 
 @login_required
+@login_required
 def user_profile_edit(request):
     context = {}
-    user_profile = get_object_or_404(UserProfile, user=request.user)
-    context['user_profile_form'] = UserProfileForm(instance=user_profile)
+    # Get or create UserProfile
+    user_profile, created = UserProfile.objects.get_or_create(user=request.user)
     
+    context['user_profile_form'] = UserProfileForm(instance=user_profile)
+
     if request.method == "POST":
         user_profile_form = UserProfileForm(request.POST, instance=user_profile)
-        user_profile_form.save()
-        return redirect('user_profile')
+        if user_profile_form.is_valid():
+            user_profile_form.save()
+            return redirect('user_profile')
+    
     return render(request, 'gstbillingapp/user_profile_edit.html', context)
+
 
 
 @login_required
@@ -120,9 +126,9 @@ def signup_view(request):
 
 @login_required
 def invoice_create(request):
-    # if business info is blank redirect to update it
-    user_profile = get_object_or_404(UserProfile, user=request.user)
-    if not user_profile.business_title:
+    # Safely get UserProfile or redirect to edit
+    user_profile = UserProfile.objects.filter(user=request.user).first()
+    if not user_profile or not user_profile.business_title:
         return redirect('user_profile_edit')
 
     context = {}
@@ -136,7 +142,6 @@ def invoice_create(request):
 
     if request.method == 'POST':
         print("POST received - Invoice Data")
-
         invoice_data = request.POST
 
         validation_error = invoice_data_validator(invoice_data)
@@ -148,43 +153,40 @@ def invoice_create(request):
         print("Valid Invoice Data")
 
         invoice_data_processed = invoice_data_processor(invoice_data)
-        # save customer
         customer = None
 
         try:
-            customer = Customer.objects.get(user=request.user,
-                                            customer_name=invoice_data['customer-name'],
-                                            customer_address=invoice_data['customer-address'],
-                                            customer_phone=invoice_data['customer-phone'],
-                                            customer_gst=invoice_data['customer-gst'])
-        except:
-            print("===============> customer not found")
-            print(invoice_data['customer-name'])
-            print(invoice_data['customer-address'])
-            print(invoice_data['customer-phone'])
-            print(invoice_data['customer-gst'])
-
-        if not customer:
-            print("CREATING CUSTOMER===============>")
-            customer = Customer(user=request.user,
+            customer = Customer.objects.get(
+                user=request.user,
                 customer_name=invoice_data['customer-name'],
                 customer_address=invoice_data['customer-address'],
                 customer_phone=invoice_data['customer-phone'],
-                customer_gst=invoice_data['customer-gst'])
-            # create customer book
+                customer_gst=invoice_data['customer-gst']
+            )
+        except Customer.DoesNotExist:
+            print("CREATING CUSTOMER===============>")
+            customer = Customer(
+                user=request.user,
+                customer_name=invoice_data['customer-name'],
+                customer_address=invoice_data['customer-address'],
+                customer_phone=invoice_data['customer-phone'],
+                customer_gst=invoice_data['customer-gst']
+            )
             customer.save()
             add_customer_book(customer)
 
-        # save product
+        # save products
         update_products_from_invoice(invoice_data_processed, request)
-
 
         # save invoice
         invoice_data_processed_json = json.dumps(invoice_data_processed)
-        new_invoice = Invoice(user=request.user,
-                              invoice_number=int(invoice_data['invoice-number']),
-                              invoice_date=datetime.datetime.strptime(invoice_data['invoice-date'], '%Y-%m-%d'),
-                              invoice_customer=customer, invoice_json=invoice_data_processed_json)
+        new_invoice = Invoice(
+            user=request.user,
+            invoice_number=int(invoice_data['invoice-number']),
+            invoice_date=datetime.datetime.strptime(invoice_data['invoice-date'], '%Y-%m-%d'),
+            invoice_customer=customer,
+            invoice_json=invoice_data_processed_json
+        )
         new_invoice.save()
         print("INVOICE SAVED")
 
@@ -193,7 +195,6 @@ def invoice_create(request):
 
         auto_deduct_book_from_invoice(new_invoice)
         print("CUSTOMER BOOK UPDATED")
-
 
         return redirect('invoice_viewer', invoice_id=new_invoice.id)
 
